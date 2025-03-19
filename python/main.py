6999afa71ef7b6b38b5e6b6ce0445463e9effd2e
Soapi14 <gdsix4ate9@gmail.com>
2025-03-19 19:15:21 -0400

    Changed font size. Plotted period vs. angle data with errorbar. No curve fit yet

--- python/main.py
+++ python/main.py
@@ -1,10 +1,40 @@
-import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib import rc
+from helper_functions import *
+from os import getcwd
 
-def plot_periods():
-    pass
+# Reformatting this so that PyCharm can autocomplete the file name
+FILE = "data_clean/symmetry_left_clean.csv"
+FILE_PATH = getcwd() + "/" + FILE
+
+def plot_periods(df):
+    # Define variables
+    periods, periods_err = average_periods(df)
+    angles = every_nth_angle(df, 10)
+    angles_err = [0.2] * len(angles)
+
+    # Plot data with error bars
+    plt.errorbar(angles, periods, periods_err, angles_err, fmt="o")
+
+    # Set plot settings
+    plt.xlabel("Angle (degrees)")
+    plt.ylabel("Period (seconds)")
+    plt.tight_layout()
+    plt.show()
 
 def main():
-    pass
+    # Define fonts
+    font = {'family' : 'DejaVu Sans',
+        'weight' : 'normal',
+        'size'   : 22}
+    rc('font', **font)
+
+    # Load csv file
+    with open(FILE_PATH, "r") as f:
+        df = pd.read_csv(f)
+
+    # Plot data
+    plot_periods(df)
 
 if __name__ == "__main__":
     main()
