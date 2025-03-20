451abc75d360858a5a1cc14002c6d644ed3145c8
Soapi14 <gdsix4ate9@gmail.com>
2025-03-20 14:57:47 -0400

    Plotting tau values with a line of best fit. Some minor changes included

--- python/main.py
+++ python/main.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 from matplotlib import rc
 from helper_functions import *
 from fit_functions import *
@@ -7,7 +8,7 @@ from os import getcwd
 FILE = "data_clean/250grams_clean.csv"
 FILE_PATH = getcwd() + "/" + FILE
 
-FUNCTION_INDEX = 1
+FUNCTION_INDEX = 0
 FIT_FUNCTIONS = [linear, quadratic, exponential]
 CURRENT_FUNCTION = FIT_FUNCTIONS[FUNCTION_INDEX]
 
@@ -16,7 +17,7 @@ def plot_periods(df):
     # Define variables
     periods, periods_err = average_periods(df)
     angles = every_nth_angle(df, 10)
-    angles_err = [0.2] * len(angles)
+    angles_err = [1.0] * len(angles)
 
     # Clean data
     outlier_management(angles, periods, periods_err, angles_err)
@@ -24,11 +25,32 @@ def plot_periods(df):
     # Plot data with error bars
     plt.errorbar(angles, periods, periods_err, angles_err, fmt="o")
 
+    # Plot line of best fit
     fit_data(CURRENT_FUNCTION, angles, periods, periods_err)
 
     # Set plot settings
-    plt.xlabel("Angle (degrees)")
+    plt.xlabel("Initial Angle (degrees)")
     plt.ylabel("Period (seconds)")
+    plt.tight_layout()
+    plt.show()
+
+
+def plot_tau(df):
+    # Define variables
+    tau, tau_err = tau_list(df, 10)
+    angles = every_nth_angle(df, 10)
+    angles = angles[:len(tau)]
+    angles_err = [1.0] * len(angles)
+
+    # Plot data with error bar
+    plt.errorbar(angles, tau, tau_err, angles_err, fmt="o")
+
+    # Plot line of best fit
+    fit_data(CURRENT_FUNCTION, angles, tau, tau_err)
+
+    # Set plot settings
+    plt.xlabel("Initial Angle (degrees)")
+    plt.ylabel("Time constant (seconds)")
     plt.tight_layout()
     plt.show()
 
@@ -45,7 +67,8 @@ def main():
         df = pd.read_csv(f)
 
     # Plot data
-    plot_periods(df)
+    # plot_periods(df)
+    plot_tau(df)
 
 if __name__ == "__main__":
     main()
