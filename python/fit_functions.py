e05ebc8854e18e925ec3c3ba5c26e5bd9e3267d8
Soapi14 <gdsix4ate9@gmail.com>
2025-03-19 21:52:16 -0400

    Defined a number of new fit functions. Added curve_fit functions. Implemented goodness of fit calculations. Added functions to plot lines of best fit

--- python/fit_functions.py
+++ python/fit_functions.py
@@ -1,4 +1,7 @@
 import numpy as np
+from scipy.optimize import curve_fit
+from scipy.stats import chi2
+import matplotlib.pyplot as plt
 
 def linear(x, m, b):
     """
@@ -12,3 +15,91 @@ def linear(x, m, b):
     m = np.array(m)
 
     return m * x + b
+
+def quadratic(x, a, b, c):
+    """
+    Quadratic function.
+    :param x: Independent variable.
+    :param a: Multiplication factor.
+    :param b: x-displacement.
+    :param c: y-displacement.
+    :return: Dependent variable.
+    """
+    a = np.array(a)
+
+    return a * (x - b)**2 + c
+
+def exponential(x, a, b, c, d):
+    """
+    Exponential function.
+    :param x: Independent variable.
+    :param a: Multiplication factor. (In front)
+    :param b: Multiplication factor. (In exponent)
+    :param c: x-displacement.
+    :param d: y-displacement.
+    :return: Dependent variable.
+    """
+    a = np.array(a)
+    b = np.array(b)
+
+    first = np.exp(b * (x - c))
+    return a * first + d
+
+def fit_curve(function, x, y, y_err):
+    """
+    Use scipy.optimize.curve_fit to fit a curve to data.
+    :param function: The function used to fit values.
+    :param x: Independent variable.
+    :param y: Dependent variable.
+    :param y_err: Error on dependent variable.
+    :return: Optimized values and uncertainties.
+    """
+    popt, pcov = curve_fit(function, x, y,
+                           sigma=y_err, absolute_sigma=True)
+
+    pcov = np.sqrt(np.diag(pcov))
+    print(f"Optimized values: {popt}\nUncertainties: {pcov}")
+
+    return popt, pcov
+
+def print_goodness_of_fit(function, popt, x, y, y_err):
+    """
+    Calculate and print goodness of fit.
+    :param function: function used to fit data.
+    :param popt: Optimized values.
+    :param x: Independent variable.
+    :param y: Dependent variable.
+    :param y_err: Error on dependent variable.
+    :return: Nothing
+    """
+    # Calculate values
+    chi_squared_fit = function(x, *popt)
+    chi_squared = np.sum(((y - chi_squared_fit) /
+                          y_err) ** 2)
+    dof = len(x) - len(popt)
+
+    # Print values
+    print(f"Chi-squared: {chi_squared}")
+    print(f"Degrees of Freedom: {dof}")
+    print(f"Chi-squared probability: {1 - chi2.cdf(chi_squared, dof)}")
+
+def fit_data(function, x, y, y_err):
+    """
+    Fit a line of best fit, and plot it on top of the data.
+    Goodness of fit is printed.
+    :param function: Function used to fit data.
+    :param x: Independent variable.
+    :param y: Dependent variable.
+    :param y_err: Error on dependent variable.
+    :return: Nothing
+    """
+    # Fit curve
+    popt, _ = fit_curve(function, x, y, y_err)
+
+    # Determine goodness of fit
+    print_goodness_of_fit(function, popt, x, y, y_err)
+
+    # Plot line of best fit
+    x_bf = np.linspace(x[0], x[-1], 1000)
+    y_bf = function(x_bf, *popt)
+    plt.plot(x_bf, y_bf)
