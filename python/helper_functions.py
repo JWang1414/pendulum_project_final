556db811c2ed7f547125a591159270e5ec082e6e
Soapi14 <gdsix4ate9@gmail.com>
2025-03-19 21:53:15 -0400

    Implemented numerous functions to help remove outliers from the data. These include changes small uncertainties to realistic ones, and removes data points with large uncertainties. Their functionalities have been combined in outlier_management

--- python/helper_functions.py
+++ python/helper_functions.py
@@ -34,34 +34,85 @@ def average_periods(df: pd.DataFrame):
         avg_periods.append(np.mean(temp))
         std_list.append(np.std(temp))
 
-    _remove_outliers_period(avg_periods, std_list, 0.1)
-
     return avg_periods, std_list
 
 def every_nth_angle(df: pd.DataFrame, n: int):
     """
     Creates a list of every nth angle in the dataframe.
+    Intended to be used in conjunction with average_periods.
     :param df: Pandas dataframe. It is assumed the second column is angular position.
     :param n: Jump length. For example, if n = 3, then every third angle will be returned
     :return: A list of every nth angle.
     """
     result = []
 
-    for i in range(0, len(df.index), n):
+    for i in range(0, len(df.index) - 1, n):
         result.append(df.iat[i, 1])
 
     return result
 
-def _remove_outliers_period(data, std, threshold):
+def control_uncertainties(std, threshold):
     """
-    Removes outliers from a list of periods. Modified in-place.
-    :param data: List of periods.
-    :param std: Standard deviation of the periods.
-    :param threshold: Threshold for outlier removal.
+    Find uncertainties that are too small and replace them with the threshold.
+    :param std: Standard deviation for each data point
+    :param threshold: Threshold for outlier detection
+    :return: Nothing
     """
-    std_copy = std.copy()
+    for i in range(len(std)):
+        if std[i] < threshold:
+            std[i] = threshold
 
-    for i in range(len(data)):
-        if std_copy[i] > threshold:
-            data.pop(i)
-            std.pop(i)
+    return
+
+def find_outliers(std, threshold):
+    """
+    Find the potential outliers in the data.
+    Based on the standard deviation.
+    :param std: Standard deviation for each data point
+    :param threshold: Threshold for outlier detection
+    :return: A list of the indices for the outliers.
+    """
+    indices = []
+
+    for i in range(len(std)):
+        if std[i] > threshold:
+            indices.append(i)
+
+    return indices
+
+def wipe_outliers(x, y, y_err, x_err):
+    """
+    Removes outliers from the data.
+    :param x: Independent variable.
+    :param y: Dependent variable.
+    :param y_err: Error on dependent variable.
+    :param x_err: Error on independent variable.
+    :return: Nothing
+    """
+    # Find outliers
+    outliers = find_outliers(y_err, 0.1)
+
+    # Remove outliers
+    i = 0
+    while i < len(x):
+        if i in outliers:
+            x.pop(i)
+            y.pop(i)
+            y_err.pop(i)
+            x_err.pop(i)
+        else:
+            i += 1
+
+    return
+
+def outlier_management(x, y, y_err, x_err):
+    """
+    Combines numerous functions to manage outliers.
+    :param x: Independent variable.
+    :param y: Dependent variable.
+    :param y_err: Error on dependent variable.
+    :param x_err: Error on independent variable.
+    :return: Nothing
+    """
+    control_uncertainties(y_err, 0.001)
+    wipe_outliers(x, y, y_err, x_err)
