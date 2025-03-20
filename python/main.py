b3edd5918335dbcf2b63aae0c5daf21135a2d144
Soapi14 <gdsix4ate9@gmail.com>
2025-03-19 21:53:50 -0400

    Implemented helper functions and fit functions. Periods for the symmetry_left dataset now have a nice fitted curve

--- python/main.py
+++ python/main.py
@@ -1,11 +1,16 @@
 import matplotlib.pyplot as plt
 from matplotlib import rc
 from helper_functions import *
+from fit_functions import *
 from os import getcwd
 
 # Reformatting this so that PyCharm can autocomplete the file name
 FILE = "data_clean/symmetry_left_clean.csv"
 FILE_PATH = getcwd() + "/" + FILE
+
+FUNCTION_INDEX = 1
+FIT_FUNCTIONS = [linear, quadratic, exponential]
+CURRENT_FUNCTION = FIT_FUNCTIONS[FUNCTION_INDEX]
 
 def plot_periods(df):
     # Define variables
@@ -13,8 +18,13 @@ def plot_periods(df):
     angles = every_nth_angle(df, 10)
     angles_err = [0.2] * len(angles)
 
+    # Clean data
+    outlier_management(angles, periods, periods_err, angles_err)
+
     # Plot data with error bars
     plt.errorbar(angles, periods, periods_err, angles_err, fmt="o")
+
+    fit_data(CURRENT_FUNCTION, angles, periods, periods_err)
 
     # Set plot settings
     plt.xlabel("Angle (degrees)")
