8352c8e92550ad18f1a12d43adf385b74aeb09be
Soapi14 <gdsix4ate9@gmail.com>
2025-04-13 19:47:40 -0400

    Checking decay fits

--- python/sample_plot.py
+++ python/sample_plot.py
@@ -1,6 +1,9 @@
 from matplotlib import pyplot as plt
 import pandas as pd
+from numpy.core.function_base import linspace
+
 from python.files import ALL_FILES
+from python.fit_functions import find_tau, fit_curve, decay_function
 
 """
 Plot a given dataset
@@ -8,15 +11,27 @@ Should be relatively simple. Exclusively for testing purposes.
 """
 
 # Define global variables
-FILE_ID = "length6"
+FILE_ID = "50g"
 
 def main():
     # Load data
     with open(ALL_FILES[FILE_ID], "r") as f:
         df = pd.read_csv(f)
 
+    # Compress variables
+    time = df["time"]
+    angle = df["angle"]
+
+    # Plot exponential decay
+    popt, pcov = fit_curve(decay_function, time, angle, [3.0] * len(angle),
+                           [angle[0], 10, 0], False)
+
+    x_bf = linspace(min(time), max(time), 100)
+    y_bf = decay_function(x_bf, *popt)
+
     # Plot data
-    plt.plot(df["time"], df["angle"], '.')
+    plt.plot(time, angle, '.')
+    plt.plot(x_bf, y_bf)
     plt.title(FILE_ID)
     plt.tight_layout()
     plt.show()
