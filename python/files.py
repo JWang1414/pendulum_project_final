25fb9baf6f7355829741e3500849e255fb0fa064
Soapi14 <gdsix4ate9@gmail.com>
2025-04-12 22:40:26 -0400

    Updates to files names

--- python/files.py
+++ python/files.py
@@ -4,28 +4,34 @@ CURRENT_DIR = getcwd() + "/"
 DATA_DIR = CURRENT_DIR + "data_clean/"
 
 ALL_FILES = {
-    "11_cm": DATA_DIR + "11cm_clean.csv",
-    "16_cm": DATA_DIR + "16cm_clean.csv",
-    "23_cm": DATA_DIR + "23cm_clean.csv",
-    "32_cm": DATA_DIR + "32cm_clean.csv",
-    "50_grams": DATA_DIR + "50grams_clean.csv",
-    "150_grams": DATA_DIR + "150grams_clean.csv",
-    "200_grams": DATA_DIR + "200grams_clean.csv",
-    "250_grams": DATA_DIR + "250grams_clean.csv",
+    "50g": DATA_DIR + "50g_clean.csv",
+    "100g": DATA_DIR + "100g_clean.csv",
+    "200g": DATA_DIR + "200g_clean.csv",
+    "300g": DATA_DIR + "300g_clean.csv",
+    "500g": DATA_DIR + "500g_clean.csv",
+    "length1": DATA_DIR + "length1_clean.csv",
+    "length2": DATA_DIR + "length2_clean.csv",
+    "length3": DATA_DIR + "length3_clean.csv",
+    "length4": DATA_DIR + "length4_clean.csv",
+    "length5": DATA_DIR + "length5_clean.csv",
+    "length6": DATA_DIR + "length6_clean.csv",
     "left": DATA_DIR + "symmetry_left_clean.csv",
     "right": DATA_DIR + "symmetry_right_clean.csv"
 }
 
 LENGTH_FILES = [
-    ALL_FILES["11_cm"],
-    ALL_FILES["16_cm"],
-    ALL_FILES["23_cm"],
-    ALL_FILES["32_cm"]
+    ALL_FILES["length1"],
+    ALL_FILES["length2"],
+    ALL_FILES["length3"],
+    ALL_FILES["length4"],
+    ALL_FILES["length5"],
+    ALL_FILES["length6"]
 ]
 
 MASS_FILES = [
-    ALL_FILES["50_grams"],
-    ALL_FILES["150_grams"],
-    ALL_FILES["200_grams"],
-    ALL_FILES["250_grams"]
+    ALL_FILES["50g"],
+    ALL_FILES["100g"],
+    ALL_FILES["200g"],
+    ALL_FILES["300g"],
+    ALL_FILES["500g"]
 ]
