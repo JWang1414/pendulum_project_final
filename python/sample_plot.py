2aa089261cba81e55ea46ba7a54c841f17b1767b
Soapi14 <gdsix4ate9@gmail.com>
2025-04-14 00:34:55 -0400

    Editing plot labels

--- python/sample_plot.py
+++ python/sample_plot.py
@@ -1,10 +1,11 @@
 import numpy as np
-from matplotlib import pyplot as plt
+from matplotlib import pyplot as plt, rc
 import pandas as pd
 from numpy.core.function_base import linspace
 
 from python.files import ALL_FILES
 from python.fit_functions import find_tau, fit_curve, decay_function
+from python.main import fig_settings
 
 """
 Plot a given dataset
@@ -12,9 +13,15 @@ Should be relatively simple. Exclusively for testing purposes.
 """
 
 # Define global variables
-FILE_ID = "200g"
+FILE_ID = "length6"
 
 def main():
+    # Define fonts
+    font = {'family' : 'DejaVu Sans',
+        'weight' : 'normal',
+        'size'   : 22}
+    rc('font', **font)
+
     # Load data
     with open(ALL_FILES[FILE_ID], "r") as f:
         df = pd.read_csv(f)
@@ -27,13 +34,16 @@ def main():
     popt, pcov = fit_curve(decay_function, time, angle, [3.0] * len(angle),
                            [angle[0], 10, 0], True)
 
+    # Calculate line of best fit
     x_bf = linspace(min(time), max(time), 100)
     y_bf = decay_function(x_bf, *popt)
 
     # Plot data
     plt.plot(time, angle, '.')
     plt.plot(x_bf, y_bf)
-    plt.title(FILE_ID)
+    plt.suptitle(FILE_ID)
+    plt.xlabel("time (seconds)")
+    plt.ylabel("amplitude (degrees)")
     plt.tight_layout()
     plt.show()
 
